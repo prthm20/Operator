b57b6f547c22e65bba41d09be2c24ec5533cee5e
prthm20 <aprathamm@gmail.com>
2026-06-16 13:13:14 +0530

    push

--- server/corsair.ts
+++ server/corsair.ts
@@ -6,14 +6,19 @@ import { gmail } from '@corsair-dev/gmail';
 import { googlecalendar } from '@corsair-dev/googlecalendar';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ 
+  connectionString: process.env.DATABASE_URL,
+  max: 2,
+  idleTimeoutMillis: 10000,
+  connectionTimeoutMillis: 5000,
+});
 
 const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({ adapter });
 
 export const corsair = createCorsair({
-  plugins: [gmail(),googlecalendar()],
+  plugins: [gmail(), googlecalendar()],
   database: pool,
   kek: process.env.CORSAIR_KEK!,
   multiTenancy: true,
